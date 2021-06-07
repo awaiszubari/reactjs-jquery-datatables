91f5a3934c0a07280836e32696064ddf51e0877e
awais7 <awaiszubari@gmail.com>
2021-06-07 12:04:37 +0500

    created datatable component

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # reactjs-jquery-datatables
 Integrating jQuery Datatables to React Js.
+
+## Installing Create React App
+`npm install -g create-react-app`
+
+## Creating New Project
+`create-react-app jquery-datatables-reactjs`
---
+++ jquery-datatables-reactjs/src/components/datatable.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../assets/css/jquery.dataTables.css';
+
+const $ = require('jquery');
+$.Datatable = require('datatables.net');
+
+export default class datatable extends Component {
+    
+    componentDidMount(){
+
+    }
+    
+    componentWillUnmount(){
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
